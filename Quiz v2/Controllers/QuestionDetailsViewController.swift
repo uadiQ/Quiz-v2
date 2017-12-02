3fe5f7a24a28138f9bc7d1c5449ec6fa4af8c259
Vadym Shoshin <shoshin.vadik@gmail.com>
2017-12-02 23:15:10 +0200

    full working advanced vesrion

--- Quiz v2/Controllers/QuestionDetailsViewController.swift
+++ Quiz v2/Controllers/QuestionDetailsViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class QuestionDetailsViewController: UIViewController {
     @IBOutlet private weak var questionLabel: UILabel!
+    @IBOutlet private var answerButtons: [UIButton]!
     
     var questionToLoad: Question?
     var pageTitle: String?
@@ -23,5 +24,36 @@ class QuestionDetailsViewController: UIViewController {
         guard let questionCategory = pageTitle else { return }
         title = questionCategory
         questionLabel.text = pageQuestion.question
+        answerButtons[0].setTitle(pageQuestion.option1, for: .normal)
+        answerButtons[1].setTitle(pageQuestion.option2, for: .normal)
+        answerButtons[2].setTitle(pageQuestion.option3, for: .normal)
+        answerButtons[3].setTitle(pageQuestion.option4, for: .normal)
+    }
+    
+    private func gratz() {
+        let gratzAlert = UIAlertController(title: "Great!", message: "That's Right!", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        gratzAlert.addAction(okAction)
+        self.present(gratzAlert, animated: true, completion: nil)
+    }
+    
+    private func tryAgain() {
+        let tryAgainAlert = UIAlertController(title: "Oops", message: "Wrong answer, try again!", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        tryAgainAlert.addAction(okAction)
+        self.present(tryAgainAlert, animated: true, completion: nil)
+    }
+    
+    @IBAction func anyAnswerButtonPressed(_ sender: UIButton) {
+        guard let pageQuestion = questionToLoad else { return }
+        let answer = pageQuestion.answer
+        
+        if sender.tag == answer {
+            gratz()
+            return
+        } else {
+            tryAgain()
+            return
+        }
     }
 }
